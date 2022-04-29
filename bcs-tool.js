bc3b1f5a925885a38e818ca1bcaced03c258e501
marktsofter <102743974+marktsofter@users.noreply.github.com>
2022-04-29 08:33:33 +0200

    adjust changes

--- bcs-tool.js
+++ bcs-tool.js
@@ -9,48 +9,47 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    function translateReport(){
-        var transDict = new Object();
-        transDict = {
-            0: { de: 'Anwesenheit', en: 'Presence' },
-            1: { de: 'Arbeitszeit', en: 'Working time' },
-            2: { de: 'Projektbuchungen', en: 'Project bookings' },
-            3: { de: 'Daueraufgaben', en: 'Permanent tasks' },
-            4: { de: 'Urlaub', en: 'Holiday' },
-            5: { de: 'Krankheit', en: 'Illness' },
-            6: { de: 'Ist', en: 'All / Actual' },
-            7: { de: 'Soll', en: 'Target' },
-            8: { de: 'Saldo', en: 'Balance' },
-        }
+  var TABLE_ID = 'group,Choices,currentdeputattable_table';
+  function translateReport() {
+    var transDict = new Object();
+    transDict = {
+      0: { de: 'Anwesenheit', en: 'Presence' },
+      1: { de: 'Arbeitszeit', en: 'Working time' },
+      2: { de: 'Projektbuchungen', en: 'Project bookings' },
+      3: { de: 'Daueraufgaben', en: 'Permanent tasks' },
+      4: { de: 'Urlaub', en: 'Holiday' },
+      5: { de: 'Krankheit', en: 'Illness' },
+      6: { de: 'Ist', en: 'All / Actual' },
+      7: { de: 'Soll', en: 'Target' },
+      8: { de: 'Saldo', en: 'Balance' },
+    };
 
-        var reportTable = document.getElementById('group,Choices,deputattable_table');
+    var reportTable = document.getElementById(TABLE_ID);
 
-        if(!reportTable){
-            console.log('translate script: Error - reportTable has changed')
-        }
-
-		Object.keys(transDict).forEach((key) => {
-			reportTable.innerHTML = reportTable.innerHTML.replace(transDict[key].de, transDict[key].en);
-		});
+    if (!reportTable) {
+      console.log('translate script: Error - reportTable has changed');
     }
 
-    function highlightAllRow(){
-        var reportTable = document.getElementById('group,Choices,deputattable_table');
+    Object.keys(transDict).forEach((key) => {
+      reportTable.innerHTML = reportTable.innerHTML.replace(transDict[key].de, transDict[key].en);
+    });
+  }
 
-        if(!reportTable){
-            console.log('translate script: Error - reportTable has changed')
-        }
+  function highlightAllRow() {
+    var reportTable = document.getElementById(TABLE_ID);
 
-        reportTable.childNodes[3].childNodes[18].style.fontWeight="bold";
-        reportTable.childNodes[3].childNodes[18].style.color="red";
+    if (!reportTable) {
+      console.log('translate script: Error - reportTable has changed');
     }
 
+    reportTable.childNodes[3].childNodes[18].style.fontWeight = 'bold';
+    reportTable.childNodes[3].childNodes[18].style.color = 'red';
+  }
 
-    // Your code here...
-    translateReport();
-    highlightAllRow();
-
+  // Your code here...
+  translateReport();
+  highlightAllRow();
 })();
